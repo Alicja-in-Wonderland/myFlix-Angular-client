b0b30e0a98d007c45fb11810f2d4a1cecdda0709
Alicja <alanotowska@yahoo.co.uk>
2023-09-20 14:06:08 +0200

    Creates a Movie Card displaying all movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -26,6 +27,13 @@ export class AppComponent {
     this.dialog.open(UserLoginFormComponent, {
       // Assigning the dialog a width
       width: '280px'
+    });
+  }
+
+  //This is the function that will open the dialog when the All Movies button is clicked
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
     });
   }
 }
